9a0dc7ee276f8a55b9c300059ce026a7817c1e3a
Alexander <64273841+Snoozebugs@users.noreply.github.com>
2024-03-12 22:15:12 +0100

    Init

---
+++ Arduino_BME280/BME280_SensorData/BME280_SensorData/Settings/SerialPortSettings.cs
@@ -0,0 +1,36 @@
+﻿using System.IO.Ports;
+
+namespace BME280_SensorData.Settings
+{
+    public class SerialPortSettings
+    {
+        public string PortName { get; set; }
+        public int BaudRate { get; set; }
+        public Parity Parity { get; set; }
+        public int DataBits { get; set; }
+        public StopBits StopBits { get; set; }
+
+        public SerialPortSettings()
+        {
+            InitSettings();
+        }
+
+        public SerialPortSettings(string portName, int baudRate, Parity parity, int dataBits, StopBits stopBits)
+        {
+            PortName = portName;
+            BaudRate = baudRate;
+            Parity = parity;
+            DataBits = dataBits;
+            StopBits = stopBits;
+        }
+
+        public void InitSettings()
+        {
+            PortName = "COM3";
+            BaudRate = 9600;
+            Parity = Parity.None;
+            DataBits = 8;
+            StopBits = StopBits.One;
+        }
+    }
+}
