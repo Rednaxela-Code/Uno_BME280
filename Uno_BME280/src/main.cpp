0761ba1387695a336aeeb018a831141022ef6142
Alexander <64273841+Snoozebugs@users.noreply.github.com>
2024-03-13 22:09:53 +0100

    Revert "Trying to clean things up, does not work ATM"
    This reverts commit 79906ef7d443a6362adafbbfaf27642da22a0e91.

--- Uno_BME280/src/main.cpp
+++ Uno_BME280/src/main.cpp
@@ -4,17 +4,28 @@
 #include <Adafruit_BME280.h>
 #include <ArduinoJson.h>
 #include <LiquidCrystal.h>
-#include <screenLCD.cpp>
 
 #define SEALEVEL_HPA (1013.25)
+#define btnRIGHT 0
+#define btnUP 1
+#define btnDOWN 2
+#define btnLEFT 3
+#define btnSELECT 4
+#define btnNONE 5
 
 Adafruit_BME280 bme;
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
+int lcdKey = 0;
+int adcKeyIn = 0;
 unsigned long delayTime;
+
 
 // put function declarations here:
 void printValues();
 void printJsonValues();
+void loopLCD();
+int readLCDButtons();
 
 void setup()
 {
@@ -41,7 +52,7 @@ void loop()
 {
   // put your main code here, to run repeatedly:
   //printJsonValues();
-  loopLCD(bme);
+  loopLCD();
 }
 
 // put function definitions here:
@@ -84,3 +95,46 @@ void printJsonValues()
   delay(delayTime);
 }
 
+int readLCDButtons()
+{
+  adcKeyIn = analogRead(0); // read the value from the sensor
+  // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+  // we add approx 50 to those values and check to see if we are close
+  // We make this the 1st option for speed reasons since it will be the most likely result
+  if (adcKeyIn > 1000)
+    return btnNONE;
+  // For V1.1 us this threshold
+  if (adcKeyIn < 50)
+    return btnRIGHT;
+  if (adcKeyIn < 250)
+    return btnUP;
+  if (adcKeyIn < 450)
+    return btnDOWN;
+  if (adcKeyIn < 650)
+    return btnLEFT;
+  if (adcKeyIn < 850)
+    return btnSELECT;
+  // For V1.0 comment the other threshold and use the one below:
+  /*
+  if (adc_key_in < 50) return btnRIGHT;
+  if (adc_key_in < 195) return btnUP;
+  if (adc_key_in < 380) return btnDOWN;
+  if (adc_key_in < 555) return btnLEFT;
+  if (adc_key_in < 790) return btnSELECT;
+  */
+  return btnNONE; // when all others fail, return this...
+}
+
+void loopLCD()
+{
+  lcd.setCursor(0, 0);
+  String humText = "Hum hPa: ";
+  lcd.print(humText += bme.readHumidity());
+  lcd.setCursor(9, 1);         // move cursor to second line "1" and 9 spaces over
+
+  lcd.setCursor(0, 1);         // move to the begining of the second line
+  lcdKey = readLCDButtons(); // read the buttons
+
+  String tempText = "Temp  C: ";
+   lcd.print(tempText += bme.readTemperature());
+}
